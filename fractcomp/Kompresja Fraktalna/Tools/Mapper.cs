b08e1d459889352297714262a38fce4cb4f86bca
mnowinski <mnowinski>
2006-12-10 17:08:28 +0000

    *** empty log message ***

--- fractcomp/Kompresja Fraktalna/Tools/Mapper.cs
+++ fractcomp/Kompresja Fraktalna/Tools/Mapper.cs
@@ -9,28 +9,37 @@ namespace FractalCompression.Tools
     {
         private double s = 0;
         private double a = 0, k = 0, d = 0, l = 0, e = 0, g = 0, h = 0, m = 0;
-        private Point k1, k2, i1, i2;
-        private double val1, val2, val3, val4;
+        private Point pk, pi;
+        private int smallDelta, bigDelta;
+        private double val1, val2, val3, val4, val5, val6, val7, val8;
 
-        //to jest zle, nie nalezy tego jeszcze uzywac
-        public Mapper(double contractivityFactor, Point k1, int val1, Point k2,
-            int val2, Point i1, int val3, Point i2, int val4)
+       public Mapper(double contractivityFactor,
+            Point pk, Point pi, int smallDelta, int bigDelta,
+            int val1, int val2, int val3, int val4,
+            int val5, int val6, int val7, int val8)
         {
             s = contractivityFactor;
-            this.i1 = i1;
-            this.i2 = i2;
-            this.k1 = k1;
-            this.k2 = k2;
+            this.pk = pk;
+            this.pi = pi;
+            this.smallDelta = smallDelta;
+            this.bigDelta = bigDelta;
             this.val1 = val1;
             this.val2 = val2;
             this.val3 = val3;
             this.val4 = val4;
+            this.val5 = val5;
+            this.val6 = val6;
+            this.val7 = val7;
+            this.val8 = val8;
             ComputeParameters();
         }
 
         private void ComputeParameters()
         {
-
+            a = smallDelta / (double)bigDelta;
+            k = pi.X - pk.X * a;
+            d = a;
+            l = pi.Y - pk.Y * d;
         }
 
         public MappedPoint MapPoint(int x, int y, double val)
